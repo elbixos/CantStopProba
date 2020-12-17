39406f1ea7845e310555ed3b146da1c01d62099c
elbixos <vpage.vincent@gmail.com>
2020-12-17 18:45:14 -0400

    7th

--- cantstop.py
+++ cantstop.py
@@ -67,6 +67,7 @@ print (compute_result (1, [6,7,8], [9,10,11,12], [6,5,6,4]))
 
 
 while True:
+    
     print("enter possible choices")
     print("a single line with numbers_of_remaining_tokens:your_lines:completed_lines")
     print("such as :")
